83cccd3dbe99d510728d997d1a12846b0a546017
Sergey Guskov <38214057+dailysergey@users.noreply.github.com>
2022-07-05 12:35:49 +0300

    loaded files

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 # This app is for educational purpose only. Insights gained is not financial advice. Use at your own risk!
 from operator import itemgetter
-from sklearn import cluster
+
 import streamlit as st
 import pandas as pd
-import base64
+
 import matplotlib.pyplot as plt
 from PIL import Image
 import requests
-import json
-import time
-from st_aggrid import AgGrid
+
 from collections import Counter, OrderedDict
 from ast import literal_eval
 import numpy as np
-from streamlit_echarts import st_echarts
-from streamlit_echarts import JsCode
-from traitlets import All
-
 
 #---------------------------------#
 # Page layout
@@ -24,6 +18,7 @@ from traitlets import All
 st.set_page_config(page_title="GISP результаты кластеризации",
                    page_icon="📈", layout="wide")
 #---------------------------------#
+main_dir = "https://github.com/dailysergey/stream-gisp-cluster/releases/download/streamlit-csv"
 
 cover_image = Image.open(requests.get(
     "https://ecpexpert.ru/wp-content/uploads/2021/05/lichnyj-kabinet-gisp-minpromtorg-funktsional-akkaunta-pravila-vosstanovleniya-parolya.jpg", stream=True).raw)
@@ -40,18 +35,18 @@ st.markdown("""
 # header
 st.markdown('Полученные метрики кластеризации для 100тыс. товаров')
 
-results = pd.read_csv("results.csv")
+results = pd.read_csv(main_dir / "results.csv")
 st.table(results)
 
 
 # Полученные шаблоны
-product_names = pd.read_csv('products_name.csv')
+product_names = pd.read_csv(main_dir / 'products_name.csv')
 
 
 @st.cache(allow_output_mutation=True)
 def get_products_df(path):
 
-    products_df = pd.read_csv(path)
+    products_df = pd.read_csv(main_dir / path)
     products_df['okpd2'].fillna(0, inplace=True)
     products_df['ktru'].fillna(0, inplace=True)
 
@@ -60,9 +55,9 @@ def get_products_df(path):
 
 @st.cache(allow_output_mutation=True)
 def get_okpd2_ktru():
-    okpd2_df = pd.read_csv("okpd2.csv", index_col="id")
+    okpd2_df = pd.read_csv(main_dir / "okpd2.csv", index_col="id")
     okpd2_df.loc[0] = {'name': "Не определено"}
-    ktru_df = pd.read_csv("ktru.csv", index_col="id")
+    ktru_df = pd.read_csv(main_dir / "ktru.csv", index_col="id")
     ktru_df.loc[0] = {'name': "Не определено"}
     return okpd2_df, ktru_df
 
@@ -140,27 +135,24 @@ group_number = ktru_products_table['group_number'].values[0]
 
 template_result = make_template(df, group_number)
 
-cluster_keys={}
+cluster_keys = {}
 for ind, item in enumerate(template_result):
     cluster_keys[ind] = list(item.keys())[0]
 
 inv_cluster_keys = {v: k for k, v in cluster_keys.items()}
 
 
-
 cluster_num = st.selectbox(f'Кластер № (Всего кластеров {len(cluster_keys)})',
                            options=list(cluster_keys.keys()),
-                           )              
+                           )
 
 real_cluter_num = cluster_keys[cluster_num]
 
 cur_template = sorted(list(template_result[cluster_num].values())[
                       0].items(), key=itemgetter(1), reverse=True)
 
-#cur_template = sorted(list(template_result[cluster_num].values())[0].items(), key=lambda x: x[1], reverse=True)
 
 st.subheader(f'Всего характеристик в данном шаблоне: {len(cur_template)}')
-
 # CSS to inject contained in a string
 hide_table_row_index = """
             <style>
@@ -176,12 +168,12 @@ character.columns = ['Тип характеристики', 'Частота вс
 st.table(character)
 
 
-st.subheader(f'Товары из этого кластера: Всего { ktru_products_table[ktru_products_table["cluster_number"] == real_cluter_num].shape[0]}')
+st.subheader(
+    f'Товары из этого кластера: Всего { ktru_products_table[ktru_products_table["cluster_number"] == real_cluter_num].shape[0]}')
 st.dataframe(
     ktru_products_table[ktru_products_table['cluster_number'] == real_cluter_num][['id', 'name_product']])
 
 
-
-
 st.subheader('Итого:')
-st.table(pd.DataFrame(ktru_products_table['cluster_number'].value_counts().rename_axis('Кластер').reset_index(name='Количество товаров')).replace({'Кластер': inv_cluster_keys}))
+st.table(pd.DataFrame(ktru_products_table['cluster_number'].value_counts().rename_axis(
+    'Кластер').reset_index(name='Количество товаров')).replace({'Кластер': inv_cluster_keys}))
